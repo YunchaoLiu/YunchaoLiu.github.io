a5ef855564d5c076e07a4866367ce95dedc908d8
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2021-09-14 15:19:46 -0500

    delete the testing folder

--- testing/projects/toxicity/ngl-master/src/constants.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file Constants
- * @author Alexander Rose <alexander.rose@weirdbyte.de>
- * @private
- */
-
-export const LeftMouseButton = 1
-export const MiddleMouseButton = 2
-export const RightMouseButton = 3
--- testing/projects/toxicity/ngl-master/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type TypedArray = (
-  Int8Array | Int16Array | Int32Array |
-  Uint8ClampedArray | Uint8Array | Uint16Array | Uint32Array |
-  Float32Array | Float64Array
-)
-
-export type NumberArray = number[] | TypedArray
-
-export type Partial<T> = { [p in keyof T]?: T[p] }
